08491f397fbfae7c629c320464e9ed6ad503fd07
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-11-02 13:24:39 +0300

    #4 drilling to html

--- djangoTutorial/mainApp/views.py
+++ djangoTutorial/mainApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+
+def contacts(request):
+    return render(request, 'mainApp/basic.html', {"values":
+                                                      ['Got a questions?',
+                                                      'chat me on facebook.com/username']})
