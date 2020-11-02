4571c6ccea789907fdaedf7af2159dac7cd8c793
Nickolay Demidov <nickolay.demidov@Mac-mini-Nickolay.local>
2020-11-02 16:49:04 +0300

    #6 pages

--- djangoTutorial/news/urls.py
+++ djangoTutorial/news/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from django.urls import path
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from news.models import Articles
 
 urlpatterns = [
     path('', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:20],
-                              template_name="news/posts.html"))
+                              template_name="news/posts.html")),
+    path('<int:pk>/', DetailView.as_view(model=Articles,
+                                            template_name="news/post.html"))
+
 ]
